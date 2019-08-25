316946751c571665d086e6e36062488cb8599a43
Lei Hua <leihua2009@gmail.com>
2019-08-25 12:06:11 -0400

    hello test programs

---
+++ 001_introduction/001_hello/helloc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World, I love C programming. My name is %s", "Lei");
+    return 0;
+}
---
+++ 001_introduction/001_hello/hellopython.py
@@ -0,0 +1,5 @@
+def say_hello(name):
+    print("Hello Python, my name is " + name)
+
+
+say_hello("Lei Hua")
