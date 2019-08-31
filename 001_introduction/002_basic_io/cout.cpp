1b48c578e1c753c033f5400e672c7126b2f9ff7a
Lei Hua <leihua2009@gmail.com>
2019-08-31 07:16:26 -0400

    basic io

---
+++ 001_introduction/002_basic_io/cout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello "
+         << "Lei, "
+         << "cout" << endl;
+
+    cout << "I love C++ programming!" << endl;
+    return 0;
+}
