b4fa38d1f37627eed44dfbef1eff6bdc648ef2b8
Lei Hua <leihua2009@gmail.com>
2019-08-31 07:20:36 -0400

    functions built-ins

---
+++ 002_functions/001_builtins/cmath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "10^2 = pow(10, 2) = " << pow(10, 2) << endl;
+
+    double x = 10.25, result;
+    result = sqrt(x);
+    cout << "Square root of " << x << " is " << result << endl;
+    return 0;
+}
